7aa8601e0bb1cc18875a459ee19648b57c90ddb3
Equallax <34425132+Equallax@users.noreply.github.com>
2021-03-23 13:42:44 +0100

    cleannn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import mysql.connector
 import tkinter as tk
+
+import mysql.connector
 
 
 class GUI:
@@ -13,10 +14,8 @@ class GUI:
         self.left_f = tk.Frame()
         self.right_f = tk.Frame()
 
-        self.left_f.grid(row=0, column=0, sticky='nsew',
-                         padx=(20, 10))
-        self.right_f.grid(row=0, column=1, sticky='nsew',
-                          padx=(10, 20))
+        self.left_f.grid(row=0, column=0, sticky='nsew', padx=(20, 10))
+        self.right_f.grid(row=0, column=1, sticky='nsew', padx=(10, 20))
 
         self.message_label = tk.Label(self.left_f, text='Bericht')
         self.message_text = tk.Text(self.left_f, width=40, height=5,
@@ -55,8 +54,9 @@ class GUI:
 
     def get_pieps(self):
         self.piep_message.delete('1.0', 'end')
-        search = self.filter_text.get("1.0", "end-1c")\
-            .replace(' ', '').split(',')
+        search = self.filter_text.get("1.0", "end-1c").replace(' ',
+                                                               '').split(
+            ',')
         if search:
             formatted = []
             search_iter = iter(search)
@@ -67,23 +67,22 @@ class GUI:
                     f" and p.bericht REGEXP \'{tag}([^A-Za-z0-9]|$)\'")
             formatted = ''.join(formatted)
 
-            cursor.execute(
-                'select p.piep_id, p.bericht, s.voornaam, '
-                's.tussenvoegsels,'
-                's.achternaam, DATE_FORMAT(p.tijd, \'%H:%i\') '
-                'from piep p '
-                'natural join student s '
-                f'where {formatted}'
-                ' order by p.piep_id desc')
+            cursor.execute('select p.piep_id, p.bericht, s.voornaam, '
+                           's.tussenvoegsels,'
+                           's.achternaam, DATE_FORMAT(p.tijd, '
+                           '\'%H:%i\') '
+                           'from piep p '
+                           'natural join student s '
+                           f'where {formatted}'
+                           ' order by p.piep_id desc')
         else:
-            cursor.execute(
-                'select p.piep_id, p.bericht, s.voornaam, '
-                's.tussenvoegsels,'
-                's.achternaam, DATE_FORMAT(p.tijd, '
-                '\'%H:%i\') '
-                'from piep p '
-                'natural join student s '
-                'order by p.piep_id desc')
+            cursor.execute('select p.piep_id, p.bericht, s.voornaam, '
+                           's.tussenvoegsels,'
+                           's.achternaam, DATE_FORMAT(p.tijd, '
+                           '\'%H:%i\') '
+                           'from piep p '
+                           'natural join student s '
+                           'order by p.piep_id desc')
 
         results = cursor.fetchall()
         self.messages = []
@@ -108,13 +107,11 @@ class GUI:
             self.piep_message.insert('end', message)
 
 
-
 if __name__ == '__main__':
-    params = {'host': '145.74.104.145', 'user': 'okcum',
-              'db': 'okcum', 'password': 'Aa657680!',
+    params = {'host': '145.74.104.145', 'user': 'okcum', 'db': 'okcum',
+              'password': 'Aa657680!',
               'auth_plugin': 'mysql_native_password'}
     with mysql.connector.connect(**params) as conn:
         with conn.cursor() as cursor:
             cursor.execute('SET autocommit = ON')
-            GUI()
-            # menu()
+            GUI()  # menu()
